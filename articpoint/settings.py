cfd764c50598c79eabea658e2f6a6e700595d6c0
matgus217 <>
2024-01-18 14:09:47 +0000

    Deployment

--- articpoint/settings.py
+++ articpoint/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['articpoint.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['articpoint.herokuapp.com', 'localhost',
+                 '8000-matgus217-artic-point-elt9e7dfiq.us2.codeanyapp.com']
 
 
 # Application definition
