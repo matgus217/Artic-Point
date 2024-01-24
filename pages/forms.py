5df20cd21e364ae7f2a193ce4232f54e68949bce
matgus217 <>
2024-01-24 11:21:09 +0000

    Add styles in css

--- pages/forms.py
+++ pages/forms.py
@@ -14,17 +14,17 @@ sittingTimes = [('3-5', '15-17'), ('5-7', '17-19'), ('7-9', '19-21'),]
 
 class Reserve_table_form(ModelForm):
     name = forms.CharField(widget=forms.TextInput(
-        attrs={'placeholder': 'Full Name', 'style': 'width: 300px;', 'class': 'form-control'}))
+        attrs={'placeholder': 'Full Name', 'style': 'width: 200px;', 'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(
-        attrs={'placeholder': 'Email', 'style': 'width: 300px;', 'class': 'form-control'}))
+        attrs={'placeholder': 'Email', 'style': 'width: 200px;', 'class': 'form-control'}))
     phone_number = forms.IntegerField(widget=forms.TextInput(
-        attrs={'placeholder': 'Phone-number', 'style': 'width: 300px;', 'class': 'form-control'}))
+        attrs={'placeholder': 'Phone-number', 'style': 'width: 200px;', 'class': 'form-control'}))
     number_of_people = forms.IntegerField(widget=forms.NumberInput(
-        attrs={'placeholder': 'Number of people', 'style': 'width: 300px;', 'class': 'form-control'}))
+        attrs={'placeholder': 'Number of people', 'style': 'width: 200px;', 'class': 'form-control'}))
     date = forms.DateField(widget=forms.NumberInput(
-        attrs={'placeholder': 'Which day do you wish to visit us?', 'class': 'form-control', 'type': 'date', 'style': 'width: 300px;'}))
+        attrs={'placeholder': 'Which day do you wish to visit us?', 'class': 'form-control', 'type': 'date', 'style': 'width: 200px;'}))
     time = forms.ChoiceField(choices=sittingTimes, widget=forms.Select(
-        attrs={'placeholder': 'What time do you want to visit us?', 'style': 'width: 300px;', 'class': 'form-control'}))
+        attrs={'placeholder': 'What time do you want to visit us?', 'style': 'width: 200px;', 'class': 'form-control'}))
 
     class Meta:
         model = Reservation
